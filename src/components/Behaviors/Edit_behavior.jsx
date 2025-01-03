e58b91e0b1e692a95bc02a35ef528724b70eae45
Mark <hendriksen-mark@hotmail.com>
2025-01-04 00:06:40 +0100

    add more automations

--- src/components/Behaviors/Edit_behavior.jsx
+++ src/components/Behaviors/Edit_behavior.jsx
@@ -10,20 +10,69 @@ import GenericText from "../GenericText/GenericText";
 import SelectMenu from "../SelectMenu/SelectMenu";
 
 const Edit_behavior = ({ HOST_IP, API_KEY, Behavior_item, closeWizard }) => {
-  const [behaviorData, setBehaviorData] = useState({
+  const initialBehaviorData = {
     script_id: Behavior_item["script_id"],
     Name: Behavior_item["metadata"]["name"],
-    hour: Behavior_item["configuration"]["when"]["time_point"]["time"]["hour"],
-    minute: Behavior_item["configuration"]["when"]["time_point"]["time"]["minute"],
-    recurrence_days: Behavior_item["configuration"]["when"]["recurrence_days"],
-    fade_in_duration: Behavior_item["configuration"]["fade_in_duration"]["seconds"],
-    turn_lights_off_after: Behavior_item["configuration"]["turn_lights_off_after"]["seconds"],
-    end_brightness: Behavior_item["configuration"]["end_brightness"],
-  });
+    hour: Behavior_item["configuration"]["when"]?.["time_point"]?.["time"]?.["hour"] || 0,
+    minute: Behavior_item["configuration"]["when"]?.["time_point"]?.["time"]?.["minute"] || 0,
+    recurrence_days: Behavior_item["configuration"]["when"]?.["recurrence_days"] || [],
+    fade_in_duration: Behavior_item["configuration"]["fade_in_duration"]?.["seconds"] || 0,
+    turn_lights_off_after: Behavior_item["configuration"]["turn_lights_off_after"]?.["seconds"] || 0,
+    end_brightness: Behavior_item["configuration"]["end_brightness"] || 0,
+    duration: Behavior_item["configuration"]["duration"]?.seconds || 0,
+    where: Behavior_item["configuration"]["where"] || [],
+    what: Behavior_item["configuration"]["what"] || [],
+    fade_out_duration: Behavior_item["configuration"]["fade_out_duration"]?.seconds || 0,
+    end_state: Behavior_item["configuration"]["end_state"] || "",
+    start_at: Behavior_item["configuration"]["when_extended"]?.["start_at"]?.["time_point"]?.["type"] || "",
+    end_at: Behavior_item["configuration"]["when_extended"]?.["end_at"]?.["time_point"]?.["type"] || "",
+  };
 
-  const [initialTime, setValue] = useState(dayjs().hour(behaviorData.hour).minute(behaviorData.minute));
-  const [fadeInTime, setFadeInTime] = useState(dayjs().hour(0).minute(behaviorData.fade_in_duration / 60));
-  const [turnOffTime, setTurnOffTime] = useState(dayjs().hour(0).minute(behaviorData.turn_lights_off_after / 60));
+  if (Behavior_item["script_id"] === "ff8957e3-2eb9-4699-a0c8-ad2cb3ede704") {
+    delete initialBehaviorData.duration;
+    delete initialBehaviorData.where;
+    delete initialBehaviorData.what;
+    delete initialBehaviorData.fade_out_duration;
+    delete initialBehaviorData.end_state;
+    delete initialBehaviorData.start_at;
+    delete initialBehaviorData.end_at;
+  } else if (Behavior_item["script_id"] === "e73bc72d-96b1-46f8-aa57-729861f80c78") {
+    delete initialBehaviorData.hour;
+    delete initialBehaviorData.minute;
+    delete initialBehaviorData.recurrence_days;
+    delete initialBehaviorData.fade_in_duration;
+    delete initialBehaviorData.turn_lights_off_after;
+    delete initialBehaviorData.end_brightness;
+    delete initialBehaviorData.fade_out_duration;
+    delete initialBehaviorData.end_state;
+    delete initialBehaviorData.start_at;
+    delete initialBehaviorData.end_at;
+  } else if (Behavior_item["script_id"] === "7e571ac6-f363-42e1-809a-4cbf6523ed72") {
+    delete initialBehaviorData.duration;
+    delete initialBehaviorData.what;
+    delete initialBehaviorData.fade_in_duration;
+    delete initialBehaviorData.turn_lights_off_after;
+    delete initialBehaviorData.end_brightness;
+    delete initialBehaviorData.start_at;
+    delete initialBehaviorData.end_at;
+  } else if (Behavior_item["script_id"] === "7238c707-8693-4f19-9095-ccdc1444d228") {
+    delete initialBehaviorData.duration;
+    delete initialBehaviorData.hour;
+    delete initialBehaviorData.minute;
+    delete initialBehaviorData.fade_in_duration;
+    delete initialBehaviorData.turn_lights_off_after;
+    delete initialBehaviorData.end_brightness;
+    delete initialBehaviorData.fade_out_duration;
+    delete initialBehaviorData.end_state;
+  }
+
+  const [behaviorData, setBehaviorData] = useState(initialBehaviorData);
+
+  const [initialTime, setinitialTime] = useState(dayjs().set('hour', behaviorData.hour).set('minute', behaviorData.minute));
+  const [fadeInTime, setFadeInTime] = useState(dayjs().set('hour', 0).set('minute', behaviorData.fade_in_duration / 60));
+  const [turnOffTime, setTurnOffTime] = useState(dayjs().set('hour', 0).set('minute', behaviorData.turn_lights_off_after / 60));
+  const [durationTime, setDurationTime] = useState(dayjs().set('hour', Math.floor(behaviorData.duration / 3600)).set('minute', Math.floor((behaviorData.duration % 3600) / 60)));
+  const [fadeOutTime, setFadeOutTime] = useState(dayjs().set('hour', 0).set('minute', behaviorData.fade_out_duration / 60));
 
   const handleChange = (key, value) => {
     setBehaviorData((prevData) => ({
@@ -42,7 +91,7 @@ const Edit_behavior = ({ HOST_IP, API_KEY, Behavior_item, closeWizard }) => {
         when: {
           ...Behavior_item.configuration.when,
           time_point: {
-            ...Behavior_item.configuration.when.time_point,
+            ...Behavior_item.configuration.when?.time_point,
             time: {
               hour: behaviorData.hour,
               minute: behaviorData.minute,
@@ -53,8 +102,57 @@ const Edit_behavior = ({ HOST_IP, API_KEY, Behavior_item, closeWizard }) => {
         fade_in_duration: { seconds: behaviorData.fade_in_duration },
         turn_lights_off_after: { seconds: behaviorData.turn_lights_off_after },
         end_brightness: behaviorData.end_brightness,
+        duration: { seconds: behaviorData.duration },
+        where: behaviorData.where,
+        what: behaviorData.what,
+        fade_out_duration: { seconds: behaviorData.fade_out_duration },
+        end_state: behaviorData.end_state,
+        when_extended: {
+          start_at: {
+            time_point: {
+              type: behaviorData.start_at,
+            },
+          },
+          end_at: {
+            time_point: {
+              type: behaviorData.end_at,
+            },
+          },
+        },
       },
     };
+
+    if (behaviorData.script_id === "ff8957e3-2eb9-4699-a0c8-ad2cb3ede704") {
+      delete updatedBehavior.configuration.duration;
+      delete updatedBehavior.configuration.where;
+      delete updatedBehavior.configuration.what;
+      delete updatedBehavior.configuration.fade_out_duration;
+      delete updatedBehavior.configuration.end_state;
+      delete updatedBehavior.configuration.when_extended;
+    } else if (behaviorData.script_id === "e73bc72d-96b1-46f8-aa57-729861f80c78") {
+      delete updatedBehavior.configuration.when;
+      delete updatedBehavior.configuration.fade_in_duration;
+      delete updatedBehavior.configuration.turn_lights_off_after;
+      delete updatedBehavior.configuration.end_brightness;
+      delete updatedBehavior.configuration.fade_out_duration;
+      delete updatedBehavior.configuration.end_state;
+      delete updatedBehavior.configuration.when_extended;
+    } else if (behaviorData.script_id === "7e571ac6-f363-42e1-809a-4cbf6523ed72") {
+      delete updatedBehavior.configuration.duration;
+      delete updatedBehavior.configuration.what;
+      delete updatedBehavior.configuration.fade_in_duration;
+      delete updatedBehavior.configuration.turn_lights_off_after;
+      delete updatedBehavior.configuration.end_brightness;
+      delete updatedBehavior.configuration.when_extended;
+    } else if (behaviorData.script_id === "7238c707-8693-4f19-9095-ccdc1444d228") {
+      delete updatedBehavior.configuration.duration;
+      delete updatedBehavior.configuration.when;
+      delete updatedBehavior.configuration.fade_in_duration;
+      delete updatedBehavior.configuration.turn_lights_off_after;
+      delete updatedBehavior.configuration.end_brightness;
+      delete updatedBehavior.configuration.fade_out_duration;
+      delete updatedBehavior.configuration.end_state;
+    }
 
     axios
       .put(`${HOST_IP}/clip/v2/resource/behavior_instance/${Behavior_item.id}`, updatedBehavior, {
@@ -97,55 +195,151 @@ const Edit_behavior = ({ HOST_IP, API_KEY, Behavior_item, closeWizard }) => {
             onChange={(e) => handleChange("Name", e)}
           />
         </div>
-        <div className="form-control">
-          <CustomTimePicker
-            label="End Time"
-            value={initialTime}
-            onChange={(newValue) => {
-              handleChange("hour", newValue.hour());
-              handleChange("minute", newValue.minute());
-            }}
-          />
-        </div>
-        <div className="form-control">
-          <CustomTimePicker
-            label="Fade In Duration (time before end time)"
-            value={fadeInTime}
-            onChange={(newValue) => {
-              handleChange("fade_in_duration", newValue.hour() * 3600 + newValue.minute() * 60);
-              setFadeInTime(newValue);
-            }}
-          />
-        </div>
-        <div className="form-control">
-          <CustomTimePicker
-            label="Turn Lights Off After (time after end time)"
-            value={turnOffTime}
-            onChange={(newValue) => {
-              handleChange("turn_lights_off_after", newValue.hour() * 3600 + newValue.minute() * 60);
-              setTurnOffTime(newValue);
-            }}
-          />
-        </div>
-        <div className="form-control">
-          <label>End Brightness (%)</label>
-          <BrightnessSlider
-            defaultValue={behaviorData.end_brightness}
-            max = "100"
-            onChange={(value) => handleChange("end_brightness", value)}
-          />
-        </div>
-        <div className="form-control">
-          <SelectMenu
-            label="Recurrence Days"
-            options={days}
-            defaultValue={selectedDays}
-            onChange={(e) => handleChange("recurrence_days", e)}
-            close={false}
-            multie={true}
-            classOptions="maxWidth"
-          />
-        </div>
+        {behaviorData.script_id === "ff8957e3-2eb9-4699-a0c8-ad2cb3ede704" && (
+          <>
+            <div className="form-control">
+              <CustomTimePicker
+                label="End Time"
+                value={initialTime}
+                onChange={(newValue) => {
+                  handleChange("hour", newValue.hour());
+                  handleChange("minute", newValue.minute());
+                }}
+              />
+            </div>
+            <div className="form-control">
+              <CustomTimePicker
+                label="Fade In Duration (time before end time)"
+                value={fadeInTime}
+                onChange={(newValue) => {
+                  handleChange("fade_in_duration", newValue.hour() * 3600 + newValue.minute() * 60);
+                  setFadeInTime(newValue);
+                }}
+              />
+            </div>
+            <div className="form-control">
+              <CustomTimePicker
+                label="Turn Lights Off After (time after end time)"
+                value={turnOffTime}
+                onChange={(newValue) => {
+                  handleChange("turn_lights_off_after", newValue.hour() * 3600 + newValue.minute() * 60);
+                  setTurnOffTime(newValue);
+                }}
+              />
+            </div>
+            <div className="form-control">
+              <label>End Brightness (%)</label>
+              <BrightnessSlider
+                defaultValue={behaviorData.end_brightness}
+                max = "100"
+                onChange={(value) => handleChange("end_brightness", value)}
+              />
+            </div>
+            <div className="form-control">
+              <SelectMenu
+                label="Recurrence Days"
+                options={days}
+                defaultValue={selectedDays}
+                onChange={(e) => handleChange("recurrence_days", e)}
+                close={false}
+                multie={true}
+                classOptions="maxWidth"
+              />
+            </div>
+          </>
+        )}
+        {behaviorData.script_id === "e73bc72d-96b1-46f8-aa57-729861f80c78" && (
+          <>
+            <div className="form-control">
+              <CustomTimePicker
+                label="Duration"
+                value={durationTime}
+                onChange={(newValue) => {
+                  handleChange("duration", newValue.hour() * 3600 + newValue.minute() * 60);
+                  setDurationTime(newValue);
+                }}
+              />
+            </div>
+            {/* Add additional fields for "where" and "what" as needed */}
+          </>
+        )}
+        {behaviorData.script_id === "7e571ac6-f363-42e1-809a-4cbf6523ed72" && (
+          <>
+            <div className="form-control">
+              <CustomTimePicker
+                label="End Time"
+                value={initialTime}
+                onChange={(newValue) => {
+                  handleChange("hour", newValue.hour());
+                  handleChange("minute", newValue.minute());
+                }}
+              />
+            </div>
+            <div className="form-control">
+              <CustomTimePicker
+                label="Fade Out Duration (time before end time)"
+                value={fadeOutTime}
+                onChange={(newValue) => {
+                  handleChange("fade_out_duration", newValue.hour() * 3600 + newValue.minute() * 60);
+                  setFadeOutTime(newValue);
+                }}
+              />
+            </div>
+            <div className="form-control">
+              <GenericText
+                label="End State"
+                type="text"
+                placeholder="End State"
+                value={behaviorData.end_state}
+                onChange={(e) => handleChange("end_state", e)}
+              />
+            </div>
+            <div className="form-control">
+              <SelectMenu
+                label="Recurrence Days"
+                options={days}
+                defaultValue={selectedDays}
+                onChange={(e) => handleChange("recurrence_days", e)}
+                close={false}
+                multie={true}
+                classOptions="maxWidth"
+              />
+            </div>
+          </>
+        )}
+        {behaviorData.script_id === "7238c707-8693-4f19-9095-ccdc1444d228" && (
+          <>
+            <div className="form-control">
+              <GenericText
+                label="Start At"
+                type="text"
+                placeholder="Start At"
+                value={behaviorData.start_at}
+                onChange={(e) => handleChange("start_at", e)}
+              />
+            </div>
+            <div className="form-control">
+              <GenericText
+                label="End At"
+                type="text"
+                placeholder="End At"
+                value={behaviorData.end_at}
+                onChange={(e) => handleChange("end_at", e)}
+              />
+            </div>
+            <div className="form-control">
+              <SelectMenu
+                label="Recurrence Days"
+                options={days}
+                defaultValue={selectedDays}
+                onChange={(e) => handleChange("recurrence_days", e)}
+                close={false}
+                multie={true}
+                classOptions="maxWidth"
+              />
+            </div>
+          </>
+        )}
         <div className="form-control">
           <GenericButton
           value="Save"
