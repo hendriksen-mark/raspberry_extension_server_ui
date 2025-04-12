7caa7b4f132a12f18e86bf2b0e250d6d19e02a4a
Mark <hendriksen-mark@hotmail.com>
2025-04-12 19:16:00 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -14,29 +14,23 @@ const App = () => {
   const HOST_IP = ""; // Pass the IP (http://x.x.x.x) of the diyHue Bridge, if running through npm start
 
   useEffect(() => {
-    if (HOST_IP) {
-      axios
-        .get(`${HOST_IP}/get-key`)
-        .then((result) => {
-          if (typeof result.data === "string" && result.data.length === 32) {
-            setAPI_KEY(result.data);
-          } else {
-            console.error(`Unable to fetch API_KEY! from ${HOST_IP}/get-key`);
-            toast.error("Unable to fetch API_KEY!");
-          }
-        })
-        .catch((error) => {
-          console.error(error);
-          toast.error(`Error occurred: ${error.message}`);
-        })
-        .finally(() => {
-          setIsLoading(false); // Ensure loading is set to false after the request
-        });
-    } else {
-      console.error("HOST_IP is not defined.");
-      toast.error("HOST_IP is not defined.");
-      setIsLoading(true);
-    }
+    axios
+      .get(`${HOST_IP}/get-key`)
+      .then((result) => {
+        if (typeof result.data === "string" && result.data.length === 32) {
+          setAPI_KEY(result.data);
+        } else {
+          console.error(`Unable to fetch API_KEY! from ${HOST_IP}/get-key`);
+          toast.error("Unable to fetch API_KEY!");
+        }
+      })
+      .catch((error) => {
+        console.error(error);
+        toast.error(`Error occurred: ${error.message}`);
+      })
+      .finally(() => {
+        setIsLoading(false); // Ensure loading is set to false after the request
+      });
   }, []);
 
   if (isLoading) {
