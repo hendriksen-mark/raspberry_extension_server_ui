63f70368c1681af71181c39d8117300d59ee5433
Mark Hendriksen <hendriksen-mark@hotmail.com>
2026-06-29 23:07:32 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,3 @@
-import { LocalizationProvider } from '@mui/x-date-pickers';
-import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-
 import Layout from "./Mainframe/Layout";
 
 const App = () => {
@@ -8,9 +5,7 @@ const App = () => {
   const HOST_IP = ""; // Pass the IP (http://x.x.x.x) of the diyHue Bridge, if running through npm start
 
   return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Layout HOST_IP={HOST_IP} />
-    </LocalizationProvider>
+    <Layout HOST_IP={HOST_IP} />
   );
 };
 
